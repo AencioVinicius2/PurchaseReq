47057ebddc5cfb1aa5b13dda48546d3912693c09
Aencio <aenpvp9@gmail.com>
2025-08-19 15:21:47 -0300

    feat(stock): add GetAllStockItems, AddStockItem methods, and AddStockItemDTO

---
+++ API/PurchaseReq/PurchaseReq/Controllers/StockItemsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PurchaseReq.Data;
+
+namespace PurchaseReq.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockItemsController : ControllerBase
+    {
+        private readonly PurchaseDbContext dbContext;
+        public StockItemsController(PurchaseDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStockItems()
+        {
+            var items = await dbContext.Stocks.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStockItem()
+        {
+
+        }
+    }
+}
---
+++ API/PurchaseReq/PurchaseReq/Models/AddStockItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PurchaseReq.Models
+{
+    public class AddStockItemDTO
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required int Code { get; set; }
+        public int Quantity { get; set; }
+    }
+}
