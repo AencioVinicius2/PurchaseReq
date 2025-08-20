1bcf95e6c6bc61f068143d99c643bb4f87ab8908
Aencio <aenpvp9@gmail.com>
2025-08-20 14:00:46 -0300

    feat(frontend) created new frontend folder

---
+++ frontend/src/app/components/component-header/component-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-header',
+  standalone: false,
+  templateUrl: './component-header.html',
+  styleUrl: './component-header.css'
+})
+export class ComponentHeader {
+
+}
